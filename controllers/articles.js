dd489ee2ebc8a315c332b6fa1f7538b4d48dbae8
Ofir Dahan <ofirdahan@Ofirs-MacBook-Air.local>
2022-09-06 22:28:39 +0300

    Add basic readme

--- controllers/articles.js
+++ controllers/articles.js
@@ -14,8 +14,6 @@ module.exports.getArticles = (req, res, next) => {
     });
 };
 
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzE3NmJjOGMwZGMwYjU0ODI3MjZkY2UiLCJpYXQiOjE2NjI0ODY0ODEsImV4cCI6MTY2MzA5MTI4MX0.3hxw_ntmqhROj5VxYxGTIM_FEY8vXYwWZXVZlD4Ol4o
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzE3OTQ3YTMwNzhhM2NjZDVjODdiMDkiLCJpYXQiOjE2NjI0ODk3NDMsImV4cCI6MTY2MzA5NDU0M30.PEuoAZLHqrzpgy_FffmzyLsOSCVNPxZhfDovLdYsMhA
 module.exports.deleteArticle = (req, res, next) => {
   const { articleId } = req.params;
   const { owner } = req.body;
@@ -28,14 +26,8 @@ module.exports.deleteArticle = (req, res, next) => {
 };
 
 module.exports.createArticle = (req, res, next) => {
-  const article = { ...req.body, owner: req.user._id };
-  console.log('article1:', article);
-  Article.create(article)
-    .then((article) => {
-      console.log('article:', article);
-
-      return res.send(article);
-    })
+  Article.create({ ...req.body, owner: req.user._id })
+    .then((article) => res.send(article))
     .catch((err) => {
       next(err);
     });
